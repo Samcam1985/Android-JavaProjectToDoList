67f27abf1237786ea06710b36e7f38ed7f13c892
Samantha Campbell <samantha.l.campbell1985@gmail.com>
2017-07-08 10:05:59 +0100

    Get title test passed in task test

--- app/src/test/java/com/example/user/todolistandroidproject/TaskTest.java
+++ app/src/test/java/com/example/user/todolistandroidproject/TaskTest.java
@@ -1,8 +1,23 @@
 package com.example.user.todolistandroidproject;
 
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 /**
  * Created by user on 08/07/2017.
  */
 
 public class TaskTest {
+
+    Task task;
+
+    @Before
+    public void before() { task = new Task("Collect Shopping",
+            "Collect click and collect from tesco Corstorphine on 14/7 between 9am and 11am");}
+
+    @Test
+    public void getTitleTest() {
+        assertEquals("Collect Shopping", task.getTitle());
+    }
 }
